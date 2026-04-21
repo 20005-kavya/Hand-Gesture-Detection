f54b7432dd550bab8496207d8658e2f7b45c8cc4
20005-kavya <154548775+20005-kavya@users.noreply.github.com>
2026-04-21 13:50:06 +0530

    Documentation is completed

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ def video():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=7860)
+    # app.run(host='0.0.0.0', port=7860)
+    app.run()
+        
